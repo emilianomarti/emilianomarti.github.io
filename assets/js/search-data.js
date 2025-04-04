2f675686286d46cc8e5ba0ce3cce6f193c71b8a5
emilianomarti <66441446+emilianomarti@users.noreply.github.com>
2025-04-04 13:24:04 +0000

    Deploying to gh-pages from @ emilianomarti/emilianomarti.github.io@945cc92cccf6fcc8d2827b15d23e212b90168638 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-amanda-larracuente-and-i-wrote-a-short-commentary-in-trends-in-genetics-highlighting-recent-work-lead-by-dr-peiwei-chen-and-colleagues-their-study-uncovers-trailblazer-a-fast-evolving-transcription-factor-that-plays-a-key-role-in-the-suppression-of-selfish-genetic-elements-in-drosophila-read-the-article-here",
+          title: 'Amanda Larracuente and I wrote a short commentary in Trends in Genetics, highlighting...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
